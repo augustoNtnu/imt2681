3be56badae6d7ed52e9655d32d621e24d840203d
augustoNtnu <augusto@stud.ntnu.no>
2017-11-02 19:26:00 +0100

    more changes for re-writen code

--- fixerticker/fixer.go
+++ fixerticker/fixer.go
@@ -3,13 +3,18 @@ package fixerticker
 import (
 	"imt2681/Handlers"
 	"time"
+	"log"
 )
 
 
 func Ticker(){
 var value = 20000
 for i:=0; i <value; i++{
-	Handlers.FixerColl.FechtAll()
+	value := Handlers.FixerColl.FechtAll()
+	if value == 0 {
+		log.Println("error with fetching all")
+	}
+
 	Handlers.Database.InvokeAll(Handlers.FixerColl)
 
 	time.Sleep(23 * time.Hour)
